3ee8d0bd5cac581316298b59ce5f8d81b3054369
wenzouquan <359945126@qq.com>
2017-05-31 09:25:02 +0800

    12

--- src/Auth.php
+++ src/Auth.php
@@ -7,93 +7,43 @@ class Auth extends Base {
 	 * 权限限制
 	 * @return
 	 */
-	public function check($user_id, $authcode) {
-		header("Content-type: text/html; charset=utf-8");
-		$addon_auth = Phpkit::BaseModel("addon_auth");
-		$addon_auth->Title = '测试4';
-		$addon_auth->Id = 4492;
-		//$addon_auth->Authcode = 'er';
-		//$addon_auth->GroupId = '2';
-		if ($addon_auth->create() == false) {
-			echo "Umh, We can't store robots right now: \n";
-			foreach ($addon_auth->getMessages() as $message) {
-				echo $message, "\n";
-			}
-		} else {
-			echo "Great, a new robot was saved successfully!";
-		}
-		//$res1 = $addon_auth->load(array('conditions' => "Id = ?1", 'bind' => array('1' => 236), 'order' => 'Id'));
-
-		// $res2 = $addon_auth->load(23);
-		//var_dump($res1->toArray());
-		//var_dump($res1->Id);
-		// $addon_auth_access = Phpkit::BaseModel("addon_auth_access");
-		// // var_dump($addon_auth_access);
-		// $res2 = $addon_auth_access->load(131);
-		// // $res2 = $addon_auth_access->load(array(
-		// // 	"conditions" => "AdminId = ?1",
-		// // 	"bind" => array(1 => 131),
-		// // 	//'order' => 'Id desc',
-		// // ));
-		// // var_dump($res2->AdminId);
-		exit();
+	public function check($UserId, $authcode) {
+		//查出权限ID
+		$this->AuthCheckMsg = array();
 		$AddonAuth = new models\AddonAuth();
-		$res = $AddonAuth->load(array(
-			"conditions" => "Id = ?1",
-			"bind" => array(1 => 244),
+		$this->AuthInfo = $AddonAuth->load(array(
+			"conditions" => "AuthCode = ?1",
+			"bind" => array(1 => $authcode),
 			'order' => 'Id desc',
 		));
-		// var_dump($res->Id);
-		// foreach ($res as $key => $value) {
-		// 	var_dump($value->Id);
-		// }
-		//var_dump($auths->IsShow2);
-		// $res = $AddonAuth::find(array(
-		// 	"id = '244'",
-		// 	"order" => "id",
-		// ));
-		var_dump($res->Id);
-		exit();
-		// Render a view and return its contents as a string
-		//$this->view->setVar("username", 'wen');
-		// $this->view->setVars(
-		// 	[
-		// 		"username" => 'wen',
-		// 	]
-		// );
-		//添加本地css资源
-		$this->assets
-			->addCss('css/style.css')
-			->addCss('css/style2.css');
-		//添加本地js资源
-		$this->assets
-			->addJs('js/jquery.js')
-			->addJs('js/bootstrap.min.js');
-		$this->view->outputCss = $this->assets->outputCss();
-		$this->view->outputJs = $this->assets->outputJs();
-		$this->view->username = 'wen';
-		//	echo $this->view->render("welcomeMail");
-
-		//var_dump($this->App->db->fetchAll($sql));
-		exit();
-		$where = "authcode in ('$authcode1','$authcode2','$authcode3')";
-		$r = BoxModel("addon_auth")->where($where)->select();
-		if (!$r) {
-			return;
-		}
-		$admin_id = session("admin_id");
-		$auths = BoxModel("addon_auth_access")->where("admin_id='$admin_id'")->find();
-		$access = explode(",", $auths['access']);
-		$falg = 1;
-		foreach ($r as $one) {
-			if (in_array($one['id'], $access)) {
-				$falg = $falg * 0;
-			}
-		}
-		if ($falg == 1) {
-			return false;
-		} else {
+		if (empty($this->AuthInfo)) {
+			$this->AuthCheckMsg['msg'] = '当前' . $AddonAuth . '权限没有配置到数据库';
 			return true;
+		}
+		$this->AuthCheckMsg['AuthInfo'] = $this->AuthInfo->toArray();
+		//用户属于的用户组
+		$AddonAuthUser = new models\AddonAuthUser();
+		$this->AuthUser = $AddonAuthUser->load($UserId);
+		if (empty($this->AuthUser)) {
+			$this->AuthCheckMsg['msg'] = '当前:' . $UserId . '用户不属于任何用户组';
+			return false;
+		}
+		$this->AuthCheckMsg['AuthUser'] = $this->AuthUser->toArray();
+		//用户组里的权限
+		$AddonAuthUserRoles = new models\AddonAuthUserRoles();
+		$this->UserRoles = $AddonAuthUserRoles->load($this->AuthUser->RoleId);
+		if (empty($this->UserRoles)) {
+			$this->AuthCheckMsg['msg'] = '当前:' . $this->AuthUser->RoleId . '用户组没有配置权限';
+			return false;
+		}
+		//验证权限id是否在权限组里
+		$this->AuthCheckMsg['UserRoles'] = $this->UserRoles->toArray();
+		$access = explode(",", $this->UserRoles->AuthIds);
+		if (in_array($this->AuthInfo->Id, $access)) {
+			return true;
+		} else {
+			$this->AuthCheckMsg['msg'] = '当前:' . $authcode . '，不在用户组里';
+			return false;
 		}
 	}
 
--- src/models/AddonAuth.php
+++ src/models/AddonAuth.php
@@ -2,26 +2,15 @@
 namespace phpkit\auth\models;
 
 class AddonAuth extends \phpkit\core\BaseModel {
-	protected $is_show;
-	public function getIsShow() {
-		return "is_show:" . $this->is_show;
-	}
-	// public function columnMap() {
-	// 	//Keys are the real names in the table and
-	// 	//the values their names in the application
-	// 	return array(
-	// 		'is_show' => 'IsShow',
-	// 		'id' => 'Id',
-	// 		'title'
-	// 	);
-	// }
-	// public function getSource() {
-	// 	return "addon_auth_access";
-	// }
-	// public function getSchema() {
-	// 	return "phpkit";
-	// }
+ 
 	public function initialize() {
 		parent::initialize();
 	}
+
+	public function columnList(){
+		 'Id'=>'Id',
+		 'Title'=>'权限组名',
+		 'Cate'=>'类型',
+	}
+
 }
